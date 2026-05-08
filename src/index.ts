e99be8b8edb72830137d9a8f82bfacd2d7fcae31
lee <nhattruongta13@gmail.com>
2026-05-08 02:56:06 -0500

    insert users feature

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,48 @@ import express from 'express'
 import cors from 'cors'
 import { Request, Response } from 'express'
 import {pool} from './db'
+import bcrypt from 'bcrypt'
 
 
 const app = express()
 const PORT = process.env.PORT
 app.use(cors())
 app.use(express.json())
+
+app.post('/register', async (req: Request, res: Response) => {
+    try{
+        const {email, password, name} = req.body
+
+        if (!email || !password){
+            return res.status(400).json({error: 'Email and password required'})
+        }
+        const password_hash = await bcrypt.hash(password, 10)
+
+        const result = await pool.query(`
+            INSERT INTO users (email, password_hash, name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, created`,
+            [email, password, name || null]
+        )
+
+        const newUser = result.rows[0]
+        res.status(201).json({
+            message: 'User created',
+            user: newUser
+        })
+    }catch(err: any){
+        if(err.code === '23505'){
+            return res.status(409).json({
+                error: 'Email already exists'
+            })
+        }
+
+        console.error(err)
+        res.status(500).json({
+            error: 'Server error'
+        })
+    }
+})
 
 app.get('/db-test', async (req: Request,res: Response) => {
     try{
